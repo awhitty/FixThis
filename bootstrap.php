5e57b2d6055cbf7d359e299bf41f75fecac383d0
Austin Whittier <austin.whitt@gmail.com>
2012-11-01 14:24:04 -0700

    Working image preview

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,31 @@
 <?php
-require 'Slim/Slim.php';
+require 'lib/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
-$app->get('/', function () {
-    echo "Hello, world";
+$app->get('/', function () use ($app) {
+    echo $app->render('index.html');
 });
 
-$app->get('/:name', function ($name) {
-    echo "Hello, $name";
+$app->post('/preview/', function () {
+	if (!isset($_FILES['image'])) {
+        echo "No image";
+        return;
+    }
+
+    // TODO: Figure out file naming conventions
+    if (file_exists("media/temp/" . $_FILES["image"]["name"])) {
+    	echo $_FILES["image"]["name"] . " already exists. ";
+    } else {
+      move_uploaded_file($_FILES["image"]["tmp_name"],
+      "media/temp/" . $_FILES["image"]["name"]);
+      // echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+    }
+
+    echo "media/temp/" . $_FILES["image"]["name"];
+});
+
+$app->get('/api/?', function ($name) {
+    echo "This is where we'll flesh out the API";
 });
 $app->run();
-
-?>
